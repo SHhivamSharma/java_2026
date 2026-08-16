f5cf4709bde8d556defed2b88bbe281906b8de00
shivam <shivamsharma9868789@gmail.com>
2026-08-17 00:06:45 +0530

    Pattern: hollow  rectangle added

--- patterns/02.java
+++ patterns/02.java
@@ -1,21 +1,24 @@
-//solid Rectangle
+//Hollow Rectangle
 
-// * * * * * * *
-// * * * * * * *
-// * * * * * * *
-// * * * * * * *
-// * * * * * * *
-// * * * * * * *
-class Main {
+// * * * * * * * 
+// *           * 
+// *           * 
+// *           * 
+// *           * 
+// * * * * * * * 
+
+public class Main {
     public static void main(String[] args) {
-       int n = 6;
+      int n = 6;
        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                System.out.print("* ");
+            for(int j = 0 ; j <= n ; j++){
+                if( i == 0 || j == 0 || i == n-1 || j == n ){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
             }
-        System.out.println();
+            System.out.println();
        }
-
     }
 }
-
