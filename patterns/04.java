5b1c7486136231516f2c299ba2efe9eb9bd122be
shivam <shivamsharma9868789@gmail.com>
2026-08-20 01:04:18 +0530

    Pattern: inverted half pyramid rotated  added

--- patterns/04.java
+++ patterns/04.java
@@ -19,3 +19,18 @@ public class Main {
       }
     }
 }
+
+// -------- code 2--------
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int n = 5;
+      for(int i = n ; i >= 0 ; i--){
+        for(int j = 0 ; j < i ; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+}
