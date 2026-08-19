a2be8d73091e15d60c3cc8fe7a0fb2a8eb6a4524
shivam <shivamsharma9868789@gmail.com>
2026-08-19 23:57:46 +0530

    Pattern: inverted half pyramid added

--- patterns/04.java
+++ patterns/04.java
@@ -1,4 +1,5 @@
 //inverted half pyramid
+// ******
 // *****
 // ****
 // ***
@@ -9,9 +10,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      int n = 5;
+      int n = 6;
       for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j <= i ; j++){
+        for(int j = 0 ; j < n - i ; j++){
             System.out.print("*");
         }
         System.out.println();
