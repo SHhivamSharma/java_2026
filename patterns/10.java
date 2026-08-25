c046b65d4ac4cb307b0af9a78514d69b57e96405
shivam <shivamsharma9868789@gmail.com>
2026-08-25 23:45:19 +0530

    Pattern: added

--- patterns/10.java
+++ patterns/10.java
@@ -5,24 +5,33 @@
 // 1 0 1 0 
 // 1 0 1 0 1 
 
-//Floyd's Triangle -
+//Patterns 
+
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-     int n = 5 ;
-     int num = 1;
-     for (int i = 0 ; i< n ; i++){
-        for(int j = 0 ; j <= i ; j++){
-            if(num == 1){
-                System.out.print(num+ " ");
-                num--;
-            }else{
-                System.out.print(num+ " ");
-                num++;
-            }
-        }
-        System.out.println();
-     }
-    }
-}   
+
+public class Solutions {
+   public static void main(String args[]) {
+       int n = 4;
+
+
+       //upper part
+       for(int i=1; i<=n; i++) {
+           for(int j=1; j<=i; j++) {
+               System.out.print("*");
+           }
+
+
+           int spaces = 2 * (n-i);
+           for(int j=1; j<=spaces; j++) {
+               System.out.print(" ");
+           }
+
+
+           for(int j=1; j<=i; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+
+
