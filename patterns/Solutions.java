b7d9cdf14b262fe49b78479754ee1b4febca89c9
shivam <shivamsharma9868789@gmail.com>
2026-09-07 00:11:10 +0530

    Patterl added

--- patterns/Solutions.java
+++ patterns/Solutions.java
@@ -1,23 +1,31 @@
+//     1
+//    2 2
+//   3 3 3
+//  4 4 4 4
+// 5 5 5 5 5
+
+
 import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+    int num = 5 ; 
+    for (int i = 1 ; i <= num ; i++){
+        int nsp  = num - i ; 
+        int nst = (2*i) - 1;
 
-public class Solutions {
-   public static void main(String args[]) {
-       int n = 5;
+        for (int j = 0 ; j < nsp ; j++){
+            System.out.print(" ");
+        }
 
-
-       for(int i=1; i<=n; i++) {
-           //spaces
-           for(int j=1; j<=n-i; j++) {
-               System.out.print(" ");
-           }
-
-
-           //numbers
-           for(int j=1; j<=i; j++) {
-               System.out.print(i+" ");
-           }
-           System.out.println();
-       }
-   }   
-}
+        for(int j = 0 ; j < nst ; j++){
+            if( j!= 0 && j%2 != 0){
+                System.out.print(" ");
+            }else{
+                System.out.print(i);
+            }
+        }
+        System.out.println();
+    }
+    }
+}   
